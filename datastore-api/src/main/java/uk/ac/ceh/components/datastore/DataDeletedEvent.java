d1dd57d8c3733e89d24bf8b714015cac0754bb1f
Christopher Johnson <cjohn@ceh.ac.uk>
2013-07-24 15:22:35 +0100

    EOFMF-78 Adding cause repository in events

--- datastore-api/src/main/java/uk/ac/ceh/components/datastore/DataDeletedEvent.java
+++ datastore-api/src/main/java/uk/ac/ceh/components/datastore/DataDeletedEvent.java
@@ -6,14 +6,20 @@ import java.util.Collection;
  *
  * @author cjohn
  */
-public class DataDeletedEvent {
+public class DataDeletedEvent<A extends DataAuthor> {
     private final Collection<String> dataAdded;
+    private final DataRepository<A> repo;
 
-    public DataDeletedEvent(Collection<String> dataAdded) {
+    public DataDeletedEvent(DataRepository<A> repo, Collection<String> dataAdded) {
+        this.repo = repo;
         this.dataAdded = dataAdded;
     }
     
     public Collection<String> getFilenames() {
         return dataAdded;
+    }
+    
+    public DataRepository<A> getDataRepository() {
+        return repo;
     }
 }
--- datastore-api/src/main/java/uk/ac/ceh/components/datastore/DataSubmittedEvent.java
+++ datastore-api/src/main/java/uk/ac/ceh/components/datastore/DataSubmittedEvent.java
@@ -6,14 +6,20 @@ import java.util.Collection;
  *
  * @author cjohn
  */
-public class DataSubmittedEvent {
+public class DataSubmittedEvent<A extends DataAuthor> {
     private final Collection<String> dataAdded;
+    private final DataRepository<A> repo;
 
-    public DataSubmittedEvent(Collection<String> dataAdded) {
+    public DataSubmittedEvent(DataRepository<A> repo, Collection<String> dataAdded) {
+        this.repo = repo;
         this.dataAdded = dataAdded;
     }
     
     public Collection<String> getFilenames() {
         return dataAdded;
+    }
+    
+    public DataRepository<A> getDataRepository() {
+        return repo;
     }
 }
