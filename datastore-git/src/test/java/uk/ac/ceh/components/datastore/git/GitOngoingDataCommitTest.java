70dd1191d59b803542898317b6e750a8b1ac6da7
Christopher Johnson <cjohn@ceh.ac.uk>
2014-06-03 15:53:07 +0100

    adding test for new method in ongoing data commit

--- datastore-git/src/test/java/uk/ac/ceh/components/datastore/git/GitOngoingDataCommitTest.java
+++ datastore-git/src/test/java/uk/ac/ceh/components/datastore/git/GitOngoingDataCommitTest.java
@@ -1,5 +1,7 @@
 package uk.ac.ceh.components.datastore.git;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import org.junit.Before;
@@ -10,6 +12,7 @@ import uk.ac.ceh.components.datastore.DataRepositoryException;
 import static org.mockito.Mockito.*;
 import uk.ac.ceh.components.datastore.DataWriter;
 import static org.junit.Assert.*;
+import org.mockito.Spy;
 
 /**
  *
@@ -18,12 +21,14 @@ import static org.junit.Assert.*;
 public class GitOngoingDataCommitTest {
 
     @Mock GitDataRepository<GitTestUser> repository;
-    @Mock Map<String, DataWriter> toWrite;
-    @Mock List<String> toDelete;
+    @Spy Map<String, DataWriter> toWrite;
+    @Spy List<String> toDelete;
     GitDataOngoingCommit<GitTestUser> ongoingCommit;
     
     @Before
     public void createMocks() {
+        toWrite = new HashMap<>();
+        toDelete = new ArrayList<>();
         MockitoAnnotations.initMocks(this);
         ongoingCommit = new GitDataOngoingCommit<>(repository, toWrite, toDelete);
     }
@@ -68,5 +73,20 @@ public class GitOngoingDataCommitTest {
         //Then
         verify(toDelete).add(fileToDelete);
         assertSame("Expected the output of datadelete to be the same ongoing commit", ongoingCommit, deleteData);
+    }
+    
+    @Test
+    public void submittedFilesContainToWriteAndToDelete() {
+        //Given
+        GitDataOngoingCommit<GitTestUser> ongoing = ongoingCommit.deleteData("toDelete")
+                                                                 .submitData("toWrite", null);
+        
+        //When
+        List<String> filesPotentiallyChanged = ongoing.getSubmittedFiles();
+        
+        //Then
+        assertEquals("Expected only two files to have changed", 2, filesPotentiallyChanged.size());
+        assertTrue("Expect to delete to be a file which could have changed", filesPotentiallyChanged.contains("toDelete") );
+        assertTrue("Expect to write to be a file which could have changed", filesPotentiallyChanged.contains("toWrite") );
     }
 }
